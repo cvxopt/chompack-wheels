7e663184f33f706a9beb1a26ffcbffb9a45ed6e9
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2018-04-26 15:45:38 +0200

    Added Windows and Linux builds

--- config.sh
+++ config.sh
@@ -6,13 +6,11 @@ TESTS_DIR="$(pwd)/chompack/tests"
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-
-    # Build dependencies
     if [ -z "$IS_OSX" ]; then
         build_openblas
-        export CVXOPT_BLAS_LIB=openblas
-        export CVXOPT_LAPACK_LIB=openblas
-        export CVXOPT_BLAS_LIB_DIR=${BUILD_PREFIX}/lib
+        export CHOMPACK_BLAS_LIB=openblas
+        export CHOMPACK_LAPACK_LIB=openblas
+        export CHOMPACK_BLAS_LIB_DIR=${BUILD_PREFIX}/lib
     fi
 }
 
